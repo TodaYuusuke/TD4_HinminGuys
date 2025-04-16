ba746e06027f2aa21992d6be462f3ed5b586cbd4
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2025-04-16 23:06:41 +0900

    タイマークラス作成

--- Class/ComboEditor/Combo.cpp
+++ Class/ComboEditor/Combo.cpp
@@ -4,3 +4,23 @@ void Combo::Init()
 {
 	
 }
+
+void Combo::Update()
+{
+
+}
+
+bool Combo::GetConditions()
+{
+	// 条件配列の中身が無ければ条件を満たしているとする
+	if (conditions_.size() == 0) { return true; }
+
+	// 配列分ループする
+	for (int i = 0; i < conditions_.size(); i++) {
+		// １つでも条件を達成していなければ早期リターン
+		if (!conditions_[i].CheckCondition()) { return false; }
+	}
+
+	// ここまで到達した場合条件を満たしているとする
+	return true;
+}
--- Class/ComboEditor/Combo.h
+++ Class/ComboEditor/Combo.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Adapter.h>
 #include <vector>
+#include "../Condition/ICondition.h"
 
 /// <summary>
 /// コンボクラス
@@ -30,21 +31,33 @@ public: // メンバ関数
 	/// </summary>
 	void Update();
 
+	/// <summary>
+	/// デバッグの
+	/// </summary>
+	void DebugGUI(const std::string id);
+
 public: // アクセッサ等
 	
 	/// <summary>
-	/// 
+	/// 開始条件の達成状況ゲッター
 	/// </summary>
-	/// <returns></returns>
+	/// <returns>コンボの開始条件を満たしているか</returns>
 	bool GetConditions();
 
 private: // メンバ変数
 
-	// 派生コンボ先
+	// 派生コンボ先配列
 	std::vector<Combo> child_;
 
-	// 開始条件
-	
+	// コンボの開始条件配列
+	std::vector<LWP::Utility::ICondition> conditions_;
 
+	// 攻撃判定開始秒数
+	float attackStartTime_ = 0.0f;
+	// 攻撃判定終了秒数
+	float attackEndTime_ = 0.0f;
+
+	// 次のコンボへの受付開始時間
+	float ReceptTime_ = 0.0f;
 };
 
