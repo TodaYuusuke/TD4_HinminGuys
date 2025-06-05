5ed86e0508c6fb14fefdf0ef21aca34db6e267ad
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2025-06-05 15:14:41 +0900

    マテリアル名変更に伴う書き換え

--- Class/Scene/GameScene.cpp
+++ Class/Scene/GameScene.cpp
@@ -50,8 +50,8 @@ void GameScene::Initialize() {
 	// 一時的に天球を生成
 	skydome.LoadShortPath("field/skydome/SkyDome.gltf");
 	skydome.worldTF.scale = { 1000.0f,1000.0f ,1000.0f };
-	skydome.materials["マテリアル"].uvTransform.rotation = { 0.0f,0.0f ,2.0f };
-	skydome.materials["マテリアル"].uvTransform.scale = { 15.0f,10.0f ,1.0f };
+	skydome.materials["SkyDomeMaterial"].uvTransform.rotation = { 0.0f,0.0f ,2.0f };
+	skydome.materials["SkyDomeMaterial"].uvTransform.scale = { 15.0f,10.0f ,1.0f };
 	skydome.SetAllMaterialLighting(false);
 #pragma endregion
 
