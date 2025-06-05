a542743d703ca145f5006a50789026ca5cdc3215
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2025-06-05 15:21:42 +0900

    背景気に食わんかったので調整2

--- Class/Scene/GameScene.cpp
+++ Class/Scene/GameScene.cpp
@@ -50,7 +50,7 @@ void GameScene::Initialize() {
 	// 一時的に天球を生成
 	skydome.LoadShortPath("field/skydome/SkyDome.gltf");
 	skydome.worldTF.scale = { 1000.0f,1000.0f ,1000.0f };
-	skydome.materials["SkyDomeMaterial"].uvTransform.scale = { 15.0f,10.0f ,1.0f };
+	skydome.materials["SkyDomeMaterial"].uvTransform.scale = { 20.0f,10.0f ,1.0f };
 	skydome.SetAllMaterialLighting(false);
 #pragma endregion
 
